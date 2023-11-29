0dc66159003cb64f7f7b2cd9094921ef331caaf4
Jahangir506 <zahangirkhan506@gmail.com>
2023-11-29 11:44:19 +0600

    admin api, delete user api create

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -44,6 +44,25 @@ async function run() {
         return res.send({message: 'user already exists', insertedId: null})
       }
       const result = await userCollection.insertOne(user);
+      res.send(result)
+    })
+
+    app.patch('/users/admin/:id', async(req, res)=> {
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const updatedDoc = {
+          $set: {
+            role: 'admin',
+          }
+        }
+        const result = await userCollection.updateOne(filter, updatedDoc)
+        res.send(result)
+    })
+
+    app.delete('/users/:id', async(req, res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
       res.send(result)
     })
 
